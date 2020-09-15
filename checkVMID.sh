bea15f620956cec8ba057fb2c0e19a12e475e372
Aaron Langley <alangley345@gmail.com>
2020-09-15 15:17:17 -0400

    updating adding validity checking

--- checkVMID.sh
+++ checkVMID.sh
@@ -5,5 +5,7 @@ echo "Which ID do you want to check for?"
 read id1
 
 #grep for the read in value, select the first word in the returned list and trim the double quotes and colon 
-#
-grep $id1 /etc/pve/.vmlist | awk '{print $1;}' | tr -d "\"":
+
+if [(grep $id1 /etc/pve/.vmlist | awk '{print $1;}' | tr -d "\"":)=$id]
+    then
+        echo "TRUE"
