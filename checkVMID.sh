ba611aa1f1f61e44da8349f65b5bc8b679abe390
Aaron Langley <alangley345@gmail.com>
2020-09-15 15:41:19 -0400

    adding testing fixtures

--- checkVMID.sh
+++ checkVMID.sh
@@ -7,6 +7,9 @@ read id1
 #grep for the read in value, select the first word in the returned list and trim the double quotes and colon set as grep1 
 grep1=$(grep $id1 /etc/pve/.vmlist | awk '{print $1;}' | tr -d "\"":)
 
+print grep1
+print id1
+
 if [[ $grep1 == $id ]]
     then
         echo "TRUE"
