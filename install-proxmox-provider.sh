9bc442b97034fe115cf191698e49e36fcc94eafb
Aaron Langley <alangley345@gmail.com>
2020-09-22 21:20:03 -0400

    fixed localization error

--- install-proxmox-provider.sh
+++ install-proxmox-provider.sh
@@ -3,17 +3,17 @@
 #install go if not already installed
 sudo dnf install go
 
-sudo git clone https://github.com/Telmate/terraform-provider-proxmox.git ~/teraform-provider-proxmox
+sudo git clone https://github.com/Telmate/terraform-provider-proxmox.git /opt/teraform-provider-proxmox
 
 #install provider and provisioner
-sudo go install ~/terraform-provider-proxmox/cmd/terraform-provider-proxmox
-sudo go install ~/terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
+sudo go install /opt/terraform-provider-proxmox/cmd/terraform-provider-proxmox
+sudo go install /opt/terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
 
 make
 
 #Copy executable to plugin
 mkdir ~/.terraform.d/plugins
-cp ~/terraform-provider-proxmox/bin/terraform-provider-proxmox ~/.terraform.d/plugins
-cp ~/terraform-provider-proxmox/bin/terraform-provisioner-proxmox ~/.terraform.d/plugins
+cp /opt/terraform-provider-proxmox/bin/terraform-provider-proxmox ~/.terraform.d/plugins
+cp /opt/terraform-provider-proxmox/bin/terraform-provisioner-proxmox ~/.terraform.d/plugins
 
-rm -Rf ~/terraform-provider-plugin
+rm -Rf /opt/terraform-provider-plugin
