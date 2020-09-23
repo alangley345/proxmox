139cdf3dadd521e8afcfbd7e5040ae8f8a68df91
Aaron Langley <alangley345@gmail.com>
2020-09-22 21:18:22 -0400

    updated install script added cleanup lines

--- install-proxmox-provider.sh
+++ install-proxmox-provider.sh
@@ -3,13 +3,17 @@
 #install go if not already installed
 sudo dnf install go
 
+sudo git clone https://github.com/Telmate/terraform-provider-proxmox.git ~/teraform-provider-proxmox
+
 #install provider and provisioner
-sudo go install github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provider-proxmox
-sudo go install github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
+sudo go install ~/terraform-provider-proxmox/cmd/terraform-provider-proxmox
+sudo go install ~/terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
 
 make
 
 #Copy executable to plugin
 mkdir ~/.terraform.d/plugins
-cp bin/terraform-provider-proxmox ~/.terraform.d/plugins
-cp bin/terraform-provisioner-proxmox ~/.terraform.d/plugins
+cp ~/terraform-provider-proxmox/bin/terraform-provider-proxmox ~/.terraform.d/plugins
+cp ~/terraform-provider-proxmox/bin/terraform-provisioner-proxmox ~/.terraform.d/plugins
+
+rm -Rf ~/terraform-provider-plugin
