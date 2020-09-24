913b00ddd49704b603483712ef3147bf038a7548
Aaron Langley <alangley345@gmail.com>
2020-09-23 20:42:02 -0400

    updated script for git clone lines

--- install-proxmox-provider.sh
+++ install-proxmox-provider.sh
@@ -2,14 +2,14 @@
 
 #install go if not already installed
 sudo dnf install go
+sudo dnf install git
 
 #clone github repo and cd into it
 git clone https://github.com/Telmate/terraform-provider-proxmox.git
-cd terraform-provider-proxmox
 
 #install provider and provisioner
-go install terraform-provider-proxmox/cmd/terraform-provider-proxmox
-go install terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
+go install github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provider-proxmox
+go install github.com/Telmate/terraform-provider-proxmox/cmd/terraform-provisioner-proxmox
 
 make
 
