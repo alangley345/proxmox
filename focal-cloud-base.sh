c791772b65cc889625b36431a887c348d49f7f46
Aaron <alangley345@gmail.com>
2022-02-25 15:21:23 -0500

    Added cloud-init line

--- focal-cloud-base.sh
+++ focal-cloud-base.sh
@@ -8,3 +8,4 @@ qm importdisk 9000 focal-server-cloudimg-amd64.img local
 rm focal-server-cloudimg-amd64.img
 qm set 9000 --scsihw virtio-scsi-pci --scsi0 local:vm-9000-disk-0
 qm set 9000 --boot c --bootdisk scsi0
+qm set 9000 --ide2 local:cloudinit
