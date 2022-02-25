a63d2656e34a69fc9608f3345811d2634065c5d1
Aaron <alangley345@gmail.com>
2022-02-25 09:47:25 -0500

    Added reference page

--- focal-cloud-base.sh
+++ focal-cloud-base.sh
@@ -1,2 +1,10 @@
-qm create 9000 --name focal-cloud --memory 4096 --sockets 1 --cores 2 --net0 virtio,bridge=vmbr0,vlanid=1010
+#reference page: https://www.clickfabric.com/deploying-a-highly-available-rke-cluster-on-proxmox/
+
+qm create 9000 --name focal-cloud --memory 4096 --sockets 1 --cores 2 --net0 virtio,bridge=vmbr0,trunks=1010
 qm set 9000 --agent enabled=1
+
+wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+qm importdisk 9000 focal-server-cloudimg-amd64.img local
+rm focal-server-cloudimg-amd64.img
+qm set 9000 --scsihw virtio-scsi-pci --scsi0 local:vm-9000-disk-0
+qm set 9000 --boot c --bootdisk scsi0
