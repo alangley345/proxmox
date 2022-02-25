996673d2670693b2f8bc34eda768079093e44fbd
Aaron <alangley345@gmail.com>
2022-02-25 15:36:57 -0500

    fixed base template

--- focal-cloud-base.sh
+++ focal-cloud-base.sh
@@ -6,7 +6,7 @@ qm set 9000 --agent enabled=1
 wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
 qm importdisk 9000 focal-server-cloudimg-amd64.img local
 rm focal-server-cloudimg-amd64.img
-qm set 9000 --scsihw virtio-scsi-pci --scsi0 local:vm-9000-disk-0
+qm set 9000 --scsihw virtio-scsi-pci --scsi0 local:9000/vm-9000-disk-0.raw
 qm set 9000 --boot c --bootdisk scsi0
 qm set 9000 --ide2 local:cloudinit
 qm template 9000
