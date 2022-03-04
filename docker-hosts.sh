6be20ae8460de136a92fabeca3af452a90847eca
Aaron <alangley345@gmail.com>
2022-03-04 15:20:29 -0500

    Updated docker-host script

--- docker-hosts.sh
+++ docker-hosts.sh
@@ -10,13 +10,13 @@ wait
 qm clone $template 203 --full --name docker03$domain
 wait
 
-qm set 201 --ipconfig0 ip6=2001:470:e107:1010::100/64,gw6=2001:470:e107:1010::100
+qm set 201 --ipconfig0 ip6=2001:470:e107:1010::100/64,gw6=2001:470:e107:1010::1
 wait
 
-qm set 202 --ipconfig0 ip6=2001:470:e107:1010::101/64,gw6=2001:470:e107:1010::101
+qm set 202 --ipconfig0 ip6=2001:470:e107:1010::101/64,gw6=2001:470:e107:1010::1
 wait
 
-qm set 203 --ipconfig0 ip6=2001:470:e107:1010::102/64,gw6=2001:470:e107:1010::102
+qm set 203 --ipconfig0 ip6=2001:470:e107:1010::102/64,gw6=2001:470:e107:1010::1
 wait
 
 for i in {201..203}; do qm resize $i scsi0 16G; done
