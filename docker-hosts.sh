d8447ab600dc92c0d4114579db6d738fea5e7e2a
Aaron Langley <alangley345@gmail.com>
2022-03-05 15:43:50 +0000

    Added more cloud-init fields

--- docker-hosts.sh
+++ docker-hosts.sh
@@ -6,6 +6,9 @@ gw6="2001:470:e107:1010::1"
 netAdd="2001:470:e107:1010"
 numHosts=3
 hostPrefix="docker"
+googleDNS1="2001:4860:4860::8888"
+googleDNS2="2001:4860:4860::8844"
+ciuser="docker"
 
 #########################################
 j=1
@@ -13,14 +16,14 @@ while [ $j -le $numHosts ];
 do
     id="`pvesh get /cluster/nextid`"
 
-    qm clone $template $id --full --name $hostPrefix$j$domain
-    wait
-    qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6
+    qm clone $template $id --full --name $hostPrefix$j.$domain
+    wait 
+    qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6 --nameserver $googleDNS1 --nameserver $googleDNS2 --searchdomain $domain --ciuser $ciuser
     wait
     qm resize $id scsi0 20G
     wait
     qm set $id --onboot=1
     wait
-    echo "VM `$id` is ready!"
+    echo "VM is ready!"
     ((j++))
 done
