ced213a31fa2588468ce247f2d665eed96769517
Aaron Langley <alangley345@gmail.com>
2022-03-05 15:14:08 +0000

    Updating doker host script for less repeating

--- docker-hosts.sh
+++ docker-hosts.sh
@@ -1,24 +1,23 @@
+#!/usr/bin/bash
+
 domain="int.greatcatlab.net"
 template="9001"
+gw6="2001:470:e107:1010::1"
+netAdd="2001:470:e107:1010"
+numHosts=3
+hostPrefix="docker"
 
-qm clone $template 201 --full --name docker01$domain
-wait
+#########################################
+j=1
+while [$j -le $numHosts]
+do
+    id="`pvesh get /cluster/nextid`"
 
-qm clone $template 202 --full --name docker02$domain
-wait
-
-qm clone $template 203 --full --name docker03$domain
-wait
-
-qm set 201 --ipconfig0 ip6=2001:470:e107:1010::100/64,gw6=2001:470:e107:1010::1
-wait
-
-qm set 202 --ipconfig0 ip6=2001:470:e107:1010::101/64,gw6=2001:470:e107:1010::1
-wait
-
-qm set 203 --ipconfig0 ip6=2001:470:e107:1010::102/64,gw6=2001:470:e107:1010::1
-wait
-
-for i in {201..203}; do qm resize $i scsi0 16G; done
-
-for i in {201..203}; do qm set $i --onboot=1; done
+    qm clone $template $id --full --name $hostPrefix$j$domain
+    wait
+    qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6
+    wait
+    qm resize $id scsi0 20G
+    wait
+    qm set $id --onboot=1
+    wait
