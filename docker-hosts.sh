5b821303ba8a1bbfcf8f1a4eaacd21da2fe793e4
Aaron Langley <alangley345@gmail.com>
2022-03-08 02:25:34 +0000

    Added ssh keys for ansible user

--- docker-hosts.sh
+++ docker-hosts.sh
@@ -4,12 +4,12 @@ domain="int.greatcatlab.net"
 template="9001"
 gw6="2001:470:e107:1010::1"
 netAdd="2001:470:e107:1010"
-numHosts=3
+numHosts=1
 hostPrefix="docker"
 googleDNS1="2001:4860:4860::8888"
 googleDNS2="2001:4860:4860::8844"
 ciuser="docker"
-sshkey="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6LgLkaa+B2Pss6CXKJpVx7vp3XJgVEKHdZ/vZzGSSU6um8mq0YRtdymAHnOW46rNzpkBTSeM4f0h+tdWen9pyN6M/oY5Glls/qM/JFEz3yH1m1VoHt5S1d5Uk1vTC8GHDHBr6dj7Mq3Vsr2mjuTtg38HxG6c8uKFV7L2wUiI0qmsfhIpcStbqnX4PhDDvOV7eDQEoGKzolB6mRgV/Pda35phsM0n+NCE/emmUbG1o47bUZ9GLf5V1EiJmAM5HVEflaOL5xctB+CmjRfW75vAdq2YqFsDG2m4PrQFocNcu8lx/q6om6hK6ULYJ5Ci0zIXY83gaBP2tn9obOtUlYOf825EWeF+ZHT0g5LDNxyBa/8JJNcd0gIlwSWkcPA5fLvfwPlXOxw50KGL9Nzs/n8woWV1ll3fD3UuSqUS+6tYyHsqbvZg1+djRoGIAqJtHD/JetwOJP52m1SaODWbXBxLhLClxLyOLjJjLnOFuQdx/wo9eTarpa7Mp6TekeNS2f3c= ansible"
+sshkey=ansible.pub
 
 #########################################
 j=1
