1731d96545cdde467048d5dab6ca3969f9a62f97
Aaron Langley <alangley345@gmail.com>
2022-03-09 15:26:08 +0000

    Turned snippet dec laration into a variable

--- docker-hosts.sh
+++ docker-hosts.sh
@@ -10,7 +10,7 @@ googleDNS1="2001:4860:4860::8888"
 googleDNS2="2001:4860:4860::8844"
 ciuser="docker"
 sshkey=ansible.pub
-cicustom=/cloud-init/docker-host.yml
+cicustom="user=local:snippets/docker-host.yml"
 
 #########################################
 j=1
@@ -20,7 +20,7 @@ do
 
     qm clone $template $id --full --name $hostPrefix$j.$domain
     wait 
-    qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6 --nameserver $googleDNS1 --nameserver $googleDNS2 --searchdomain $domain --sshkey $sshkey --ciuser $ciuser --cicustom "user=local:snippets/docker-host.yml"
+    qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6 --nameserver $googleDNS1 --nameserver $googleDNS2 --searchdomain $domain --sshkey $sshkey --ciuser $ciuser --cicustom $cicustom
     wait
     qm resize $id scsi0 20G
     wait
