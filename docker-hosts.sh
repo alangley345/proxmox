0faf6e004499e4173e4f50697d7cf7e6efd9a6e5
Aaron Langley <alangley345@gmail.com>
2022-03-09 18:56:18 +0000

    Sorting out the resizing issue

--- docker-hosts.sh
+++ docker-hosts.sh
@@ -4,12 +4,12 @@ domain="int.greatcatlab.net"
 template="9001"
 gw6="2001:470:e107:1010::1"
 netAdd="2001:470:e107:1010"
-numHosts=3
+numHosts=1
 hostPrefix="docker"
 googleDNS1="2001:4860:4860::8888"
 googleDNS2="2001:4860:4860::8844"
 ciuser="docker"
-sshkey=ansible.pub
+sshkey=alangley345@myshell.pub
 cicustom="user=local:snippets/docker-host.yml"
 
 #########################################
@@ -21,8 +21,8 @@ do
     qm clone $template $id --full --name $hostPrefix$j.$domain
     wait 
     qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6 --nameserver $googleDNS1 --nameserver $googleDNS2 --searchdomain $domain --sshkey $sshkey --ciuser $ciuser --cicustom $cicustom
-    wait
-    qm resize $id scsi0 20G
+    sleep 15
+    qm resize $id scsi0 +15G
     sleep 15
     qm set $id --onboot=1
     wait
