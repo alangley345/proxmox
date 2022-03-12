490a2e04370249f687f5eccd0810ea830fca00bd
Aaron Langley <alangley@gmail.com>
2022-03-11 22:32:34 -0500

    Finally got the syntax for Proxmoxer figured out

--- proxmoxer/test.py
+++ proxmoxer/test.py
@@ -1,9 +1,12 @@
+from pickle import TRUE
+from telnetlib import STATUS
+from venv import create
 import config
 from proxmoxer import ProxmoxAPI
 
 proxmox = ProxmoxAPI(config.proxmoxHost, user=config.user, token_name=config.tokenName, token_value=config.tokenValue)
 
-node = proxmox.nodes('proxmox')
+myNode = proxmox.nodes('proxmox')
 vmID = proxmox.cluster.nextid.get()
 ip6  = "2001:470:e107:1010:0101:0000:0000:0000/64"
 gw6  = "2001:470:e107:1010:0000:0000:0000:0001"
@@ -12,7 +15,7 @@ tag  = 1010
 net0 =('name=eth0.%s,bridge=vmbr1,ip6=%s,gw6=%s,tag=%s' % (tag,ip6,gw6,tag))
 print(net0)
 
-node.lxc.create(vmid=vmID,
+myNode.lxc.create(vmid=vmID,
     ostemplate='local:vztmpl/fedora-35-default_20211111_amd64.tar.xz',
     hostname='fedora-test',
     storage='local',
@@ -20,7 +23,10 @@ node.lxc.create(vmid=vmID,
     swap=512,
     cores=1,
     password='secret',
-    net0=net0)
+    net0=net0,
+    unprivileged=1)
 
-proxmox.lxc.status.start(vmID)
+#myNode.lxc(vmID).status.start.create()
+
+#{node}/lxc/{vmid}/status/start
 
