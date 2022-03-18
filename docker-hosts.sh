10371870666359924d2f618532e0826e992dce74
Aaron Langley <alangley345@gmail.com>
2022-03-18 15:35:30 +0000

    Have unused docs

--- docker-hosts.sh
+++ docker-hosts.sh
@@ -5,11 +5,13 @@ template="9001"
 gw6="2001:470:e107:1010::1"
 netAdd="2001:470:e107:1010"
 numHosts=1
+
+
 hostPrefix="docker"
-googleDNS1="2001:4860:4860::8888 2001:4860:4860::8844"
-#googleDNS2="2001:4860:4860::8844"
+googleDNS1="2001:4860:4860::8888"
+googleDNS2="2001:4860:4860::8844"
 ciuser="docker"
-sshkey=alangley345@myshell.pub
+sshkey=20220309_ansible.pub
 cicustom="user=local:snippets/docker-host.yml"
 
 #########################################
@@ -20,7 +22,7 @@ do
 
     qm clone $template $id --full --name $hostPrefix$j.$domain
     wait 
-    qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6 --nameserver $googleDNS1 --nameserver $googleDNS2 --searchdomain $domain --sshkey $sshkey --ciuser $ciuser #--cicustom $cicustom
+    qm set $id --ipconfig0 ip6=$netAdd:$id::/64,gw6=$gw6 --nameserver "$googleDNS1 $googleDNS1" --searchdomain $domain --sshkey $sshkey --ciuser $ciuser #--cicustom $cicustom
     wait
     qm resize $id scsi0 +15G
     wait
